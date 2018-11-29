24285e638f1bd116c2de2c61aef7705fec3ad397
xiaoxiao <jiajianj2057@qq.com>
2018-11-29 13:58:05 +0800

    新增RESTful风格API

---
+++ DotNetStackLibraries/AspNet.WebApi.RESTful/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using AspNet.WebApi.RESTful.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AspNet.WebApi.RESTful.Controllers
+{
+    /// <summary>
+    /// RESTful：Representational state transfer 表述性状态转移
+    /// </summary>
+    public class ProductController : ApiController
+    {
+        /// <summary>
+        /// 可以使用 Get 开头表示[HttpGet]
+        /// Get: api/product
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<ProductDto> GetAllProducts() => null;
+
+        /// <summary>
+        /// Get: api/product/4
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ProductDto GetProductById(int id) => null;
+
+        /// <summary>
+        /// Post: api/product
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns></returns>
+        public IHttpActionResult PostProduct(ProductDto dto) => Ok();
+
+        /// <summary>
+        /// Delete: api/product/4
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public IHttpActionResult DeleteProduct(int id) => Ok();
+    }
+}
---
+++ DotNetStackLibraries/AspNet.WebApi.RESTful/Dtos/ProductDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNet.WebApi.RESTful.Dtos
+{
+    public class ProductDto
+    {
+    }
+}
