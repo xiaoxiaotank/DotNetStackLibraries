304c5eaef7b4166b4cc3e46ae4e0e98c6ac94fa2
xiaoxiao <jiajianj2057@qq.com>
2018-12-26 16:29:17 +0800

    新增非对称加密RSA jwt

---
+++ DotNetStackLibraries/AspNetCore.WebApi.Authentication.IdentityProvider/Controllers/AccountController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using AspNetCore.WebApi.Authentication.IdentityProvider.Dtos;
+using IdentityModel;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+
+namespace AspNetCore.WebApi.Authentication.IdentityProvider.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IHostingEnvironment _env;
+
+        public AccountController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        // GET api/account
+        [HttpPost]
+        public ActionResult<string> Login([FromBody]LoginDto dto)
+        {
+            if (!dto.UserName.Equals(dto.Password))
+            {
+                return string.Empty;
+            }
+
+            var user = new User
+            {
+                Id = 1,
+                Name = dto.UserName,
+                Email = "jjj@my.me",
+                Birthday = DateTime.Now.AddYears(-10),
+                PhoneNumber = "18888888888"
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var rsaPrivateKey = System.IO.File.ReadAllText(Path.Combine(_env.ContentRootPath, "private.key"));
+            var rSAParameters = JsonConvert.DeserializeObject<RSAParameters>(rsaPrivateKey);
+            var rsaSecurityKey = new RsaSecurityKey(rSAParameters);
+
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(new Claim[] {
+                    new Claim(JwtClaimTypes.Id, user.Id.ToString()),
+                    new Claim(JwtClaimTypes.Name, user.Name),
+                    new Claim(JwtClaimTypes.Email, user.Email),
+                    new Claim(JwtClaimTypes.PhoneNumber, user.PhoneNumber),
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(15),
+                SigningCredentials = new SigningCredentials(rsaSecurityKey, SecurityAlgorithms.RsaSha256Signature),
+                Audience = "aspnetcoreweb",
+                Issuer = "jjj",
+            };
+            var token = tokenHandler.CreateEncodedJwt(tokenDescriptor);
+
+            return token;
+        }
+
+        [HttpGet]
+        public IActionResult GenerateAndSaveKey()
+        {
+            RSAParameters publicKeys, privateKeys;
+
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                try
+                {
+                    privateKeys = rsa.ExportParameters(true);
+                    publicKeys = rsa.ExportParameters(false);
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
+            }
+
+            string dir = _env.ContentRootPath;
+
+            System.IO.File.WriteAllText(Path.Combine(dir, "private.key"), JsonConvert.SerializeObject(privateKeys));
+            System.IO.File.WriteAllText(Path.Combine(dir, "public.key"), JsonConvert.SerializeObject(publicKeys));
+
+            return Ok();
+        }
+    }
+
+    public class User
+    {
+        public int Id { get; internal set; }
+        public string Name { get; internal set; }
+        public string Email { get; internal set; }
+        public DateTime Birthday { get; internal set; }
+        public string Password { get; internal set; }
+        public string PhoneNumber { get; internal set; }
+    }
+}
---
+++ DotNetStackLibraries/AspNetCore.WebApi.Authentication.JwtBearer-RSA/Startup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using IdentityModel;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+
+namespace AspNetCore.WebApi.Authentication.JwtBearer_RSA
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
+        {
+            Configuration = configuration;
+            HostingEnvironment = hostingEnvironment;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public IHostingEnvironment HostingEnvironment { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    //var key = Encoding.ASCII.GetBytes(Consts.Secret);
+
+                    var rsaPublicKey = File.ReadAllText(Path.Combine(HostingEnvironment.ContentRootPath, "public.key"));
+                    var rsaParameters = JsonConvert.DeserializeObject<RSAParameters>(rsaPublicKey);
+                    var rsaSecurityKey = new RsaSecurityKey(rsaParameters);
+
+                    options.TokenValidationParameters.IssuerSigningKey = rsaSecurityKey;
+                    options.TokenValidationParameters.ValidAudience = "aspnetcoreweb";
+                    options.TokenValidationParameters.ValidIssuer = "jjj";
+                    options.TokenValidationParameters.NameClaimType = JwtClaimTypes.Name;
+                    //如果要使用微软基于角色的身份验证，一定要使用微软的ClaimTypes
+                    options.TokenValidationParameters.RoleClaimType = ClaimTypes.Role;
+                });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
