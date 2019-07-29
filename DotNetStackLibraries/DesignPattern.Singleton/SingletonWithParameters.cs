c52c10ef72a892d06141ec943addb187c8a195bb
xiaoxiao <jiajianj2057@qq.com>
2019-07-29 11:28:36 +0800

    feat: 新增单例设计模式

---
+++ DotNetStackLibraries/DesignPattern.Singleton/SingletonWithParameters.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Singleton
+{
+    class SingletonWithParameters
+    {
+        private static readonly object _syncLock = new object();
+        private static SingletonWithParameters _instance;
+
+        private SingletonWithParameters() { }
+
+        public int X { get; private set; }
+
+        public int Y { get; private set; }
+
+        public static SingletonWithParameters GetInstance(int x, int y)
+        {
+            if (_instance == null)
+            {
+                lock (_syncLock)
+                {
+                    if(_instance == null)
+                    {
+                        _instance = new SingletonWithParameters()
+                        {
+                            X = x,
+                            Y = y
+                        };
+                    }
+                    else
+                    {
+                        Initialize();
+                    }
+                }
+            }
+            else
+            {
+                Initialize();
+            }
+
+            return _instance;
+
+            void Initialize()
+            {
+                _instance.X = x;
+                _instance.Y = y;
+            }
+        }
+    }
+}
