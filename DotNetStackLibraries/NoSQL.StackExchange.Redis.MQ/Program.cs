85c6cb18b0e5936caf0d3bf18ac9000bd0483b41
xiaoxiao <jiajianj2057@qq.com>
2019-08-19 16:58:24 +0800

    feat: 创建Redis.MQ项目

---
+++ DotNetStackLibraries/NoSQL.StackExchange.Redis.MQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NoSQL.StackExchange.Redis.MQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
