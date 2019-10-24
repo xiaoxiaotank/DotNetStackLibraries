730581874d26c8fea4c97f7a2f77b3d798604a9d
jiajianjun <xiaoxiaotank.j.jia@yzw.cn>
2019-10-24 17:38:51 +0800

    新增集合Demo

---
+++ DotNetStackLibraries/CSharp.Collection.Principle/MyCollection.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.Collection.Principle
+{
+    /// <summary>
+    /// 简洁的实现方式（利用了foreach语法糖）
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class MyCollection1<T> : IEnumerable, IEnumerable<T>
+    {
+        private T[] _data;
+
+        public MyCollection1(T[] data)
+        {
+            _data = data;
+            if(_data == null)
+            {
+                _data = Array.Empty<T>();
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var i in _data)
+            {
+                yield return i;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    /// <summary>
+    /// 初始的原始方式（不使用foreach语法糖）
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class MyCollection2<T> : IEnumerable, IEnumerable<T>
+    {
+        private T[] _data;
+
+        public MyCollection2(T[] data)
+        {
+            _data = data;
+            if (_data == null)
+            {
+                _data = Array.Empty<T>();
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public struct Enumerator : IEnumerator, IEnumerator<T>
+        {
+            private readonly MyCollection2<T> _collection;
+            private int _index;
+
+            public T Current => _collection._data[_index]; 
+
+            object IEnumerator.Current => Current;
+
+            internal Enumerator(MyCollection2<T> collection)
+            {
+                _collection = collection;
+                _index = -1;
+            }
+
+            public bool MoveNext() => ++_index < _collection._data.Length;
+
+            public void Reset()
+            {
+                _index = -1;
+            }
+
+            public void Dispose()
+            {
+                
+            }
+        }
+    }
+}
---
+++ DotNetStackLibraries/CSharp.Collection.Principle/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CSharp.Collection.Principle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestMyCollection();
+        }
+
+        static void TestMyCollection()
+        {
+            var collection1 = new MyCollection1<int>(new[] { 1, 2, 3, 4, 5 });
+            var collection2 = new MyCollection2<int>(new[] { 6, 7, 8, 9, 10 });
+
+            #region collection1
+            foreach (var item in collection1)
+            {
+                Console.WriteLine(item);
+            }
+
+            var rator1 = collection1.GetEnumerator();
+            while (rator1.MoveNext())
+            {
+                Console.WriteLine(rator1.Current);
+            }
+
+            #endregion
+            Console.WriteLine("----------------------------------------");
+            #region collection2
+            foreach (var item in collection2)
+            {
+                Console.WriteLine(item);
+            }
+
+            var rator2 = collection2.GetEnumerator();
+            while (rator2.MoveNext())
+            {
+                Console.WriteLine(rator2.Current);
+            }
+            #endregion
+        }
+    }
+}
