d43b153b80aee56f94d2ad51a009b4be1a34900b
jiajianjun <xiaoxiaotank.j.jia@yzw.cn>
2019-11-12 10:51:03 +0800

    新增文件上传项目

---
+++ DotNetStackLibraries/MyApps.UploadFiles/Controllers/SimpleUploadFileController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApps.UploadFiles.Controllers
+{
+    /// <summary>
+    /// 简单地文件上传：
+    ///     - 需要整个文件全部上传至服务器后才能开始处理
+    ///     - 内存最大占用65kb，当超出时，旧的数据会被持久化到磁盘（虚拟内存），新的数据会存储到内存
+    ///  缺点：无法边上传边处理，数据持久化到磁盘会降低I/O速度，不适用于大文件上传
+    /// </summary>
+    public class SimpleUploadFileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            await Task.CompletedTask;
+            return View();
+        }
+
+        public async Task<IActionResult> UploadFiles(IEnumerable<IFormFile> files)
+        {
+            await Task.CompletedTask;
+            return View();
+        }
+
+        public async Task<IActionResult> UploadFiles(IFormFile file1, IFormFile file2)
+        {
+            await Task.CompletedTask;
+            return View();
+        }
+    }
+}
---
+++ DotNetStackLibraries/MyApps.UploadFiles/Controllers/StreamedUploadFileController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApps.UploadFiles.Controllers
+{
+    /// <summary>
+    /// 基于文件流的上传：
+    ///     - 利用滑动窗口的原理，边上传边处理
+    ///  优点：可用于大文件上传且速度快，内存占用度低
+    /// </summary>
+    public class StreamedUploadFileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        } 
+    }
+}
