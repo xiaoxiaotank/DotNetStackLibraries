fe9fa38fcd6517576f97adf29b5fa21916b54ffc
jiajianjun <xiaoxiaotank.j.jia@yzw.cn>
2019-12-20 13:55:34 +0800

    新增kafka demo

---
+++ DotNetStackLibraries/Kafka.Consumer/Program.cs
@@ -0,0 +1,50 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+namespace Kafka.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using(var c = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                c.Subscribe(new[] { "test-topic", "test-topic1" });
+                using var cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) =>
+                {
+                    e.Cancel = true;
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var cr = c.Consume(cts.Token);
+                            Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+                        }
+                        catch(ConsumeException ex)
+                        {
+                            Console.WriteLine($"Error occured: {ex.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    c.Close();
+                }
+            }
+        }
+    }
+}
---
+++ DotNetStackLibraries/Kafka.Producer/Program.cs
@@ -0,0 +1,32 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading.Tasks;
+
+namespace Kafka.Producer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var config = new ProducerConfig() { BootstrapServers = "localhost:9092" };
+
+            using(var p = new ProducerBuilder<Null, string>(config).Build())
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var dr = await p.ProduceAsync("test-topic", new Message<Null, string> { Value = "test" });
+                        var dr1 = await p.ProduceAsync("test-topic1", new Message<Null, string> { Value = "test1" });
+                        Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'{Environment.NewLine}Delivered '{dr1.Value}' to '{dr1.TopicPartitionOffset}'");
+                    }
+                    catch(ProduceException<Null, string> ex)
+                    {
+                        Console.WriteLine($"Delivery failed: {ex.Error.Reason}");
+                    }
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
