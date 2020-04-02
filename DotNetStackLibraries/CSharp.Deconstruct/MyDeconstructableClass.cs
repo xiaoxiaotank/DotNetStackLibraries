92a69d03e095c2c4dd82b69e1579d107d29b5be1
jiajianjun <xiaoxiaotank.j.jia@yzw.cn>
2020-04-02 17:02:37 +0800

    解构新增示例

--- DotNetStackLibraries/CSharp.Deconstruct/MyDeconstructableClass.cs
+++ DotNetStackLibraries/CSharp.Deconstruct/MyDeconstructableClass.cs
@@ -10,10 +10,19 @@ namespace CSharp.Deconstruct
 
         public int B => 2;
 
+        public string C => "jjj";
+
         public void Deconstruct(out int a, out int b)
         {
             a = A;
             b = B;
+        }
+
+        public void Deconstruct(out int a, out int b, out string c)
+        {
+            a = A;
+            b = B;
+            c = C;
         }
     }
 }
--- DotNetStackLibraries/CSharp.Deconstruct/Program.cs
+++ DotNetStackLibraries/CSharp.Deconstruct/Program.cs
@@ -2,12 +2,16 @@
 
 namespace CSharp.Deconstruct
 {
+    /// <summary>
+    /// 解构函数重载只支持参数个数不同的重载
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
             var myClass = new MyDeconstructableClass();
-            var (a, b) = myClass;
+            var (a1, b1) = myClass;
+            var (a2, b2, c2) = myClass;
 
             Console.WriteLine("Hello World!");
         }
