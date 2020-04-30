2ddf2d514c7d0466badb3eeafbe479ddf4ed4036
jiajianjun <xiaoxiaotank.j.jia@yzw.cn>
2020-04-30 10:20:58 +0800

    新增WPF mvvm login demo

---
+++ DotNetStackLibraries/WPF.MVVM.Login/ControlAttachProperty.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF.MVVM.Login
+{
+    class ControlAttachProperty
+    {
+        private static readonly Type _ownerType = typeof(ControlAttachProperty);
+
+        #region Placeholder 占位符
+        public static readonly DependencyProperty PlaceholderProperty = DependencyProperty
+            .RegisterAttached("Placeholder", typeof(string), _ownerType, new FrameworkPropertyMetadata(string.Empty));
+
+        public static string GetPlaceholder(DependencyObject obj)
+        {
+            return (string)obj.GetValue(PlaceholderProperty);
+        }
+
+        public static void SetPlaceholder(DependencyObject obj, string value)
+        {
+            obj.SetValue(PlaceholderProperty, value);
+        }
+        #endregion
+    }
+}
---
+++ DotNetStackLibraries/WPF.MVVM.Login/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WPF.MVVM.Login
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string _userName;
+
+        public string UserName
+        {
+            get => _userName;
+            set
+            {
+                _userName = value;
+                RaisePropertyChanged(nameof(UserName));
+            }
+        }
+
+        private string _password;
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                RaisePropertyChanged(nameof(Password));
+            }
+        }
+
+        public ICommand Login => new RelayCommand(() =>
+        {
+            if (UserName == "jjj" && Password == "123")
+            {
+                MessageBox.Show("登录成功");
+            }
+            else
+            {
+                MessageBox.Show("登录失败");
+            }
+        });
+    }
+}
