34f4aef0ef0cfc7a1738bd33282b0fc5d64b7449
jiajianjun <xiaoxiaotank.j.jia@yzw.cn>
2021-06-28 14:07:37 +0800

    新增xml操作

---
+++ DotNetStackLibraries/CSharp.XML.Serialize/FormModel.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace CSharp.XML.Serialize
+{
+    [XmlRoot("form")]
+    public class FormModel
+    {
+        [XmlAttribute("version")]
+        public string Version { get; set; }
+
+        [XmlElement("export")]
+        public List<ExportModel> Export { get; set; }
+
+    }
+
+    public class ExportModel
+    {
+        [XmlElement("base")]
+        public BaseModel Base { get; set; }
+
+        [XmlElement("header")]
+        public HeaderModel Header { get; set; }
+
+        [XmlElement("body")]
+        public BodyModel Body { get; set; }
+    }
+
+    public class BaseModel
+    {
+        [XmlAttribute("id")]
+        public string Id { get; set; }
+
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+    }
+
+    public class HeaderModel
+    {
+        [XmlElement("column")]
+        public List<HeaderColumnModel> Columns { get; set; }
+    }
+
+    public class HeaderColumnModel
+    {
+        [XmlAttribute("id")]
+        public string Id { get; set; }
+
+        [XmlAttribute("type")]
+        public HeaderColumnType Type { get; set; }
+
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [XmlAttribute("nullable")]
+        public bool Nullable { get; set; }
+
+        [XmlAttribute("length")]
+        public string Length { get; set; }
+
+    }
+
+    public class BodyModel
+    {
+        [XmlElement("column")]
+        public List<BodyColumnModel> Columns { get; set; }
+    }
+
+    public class BodyColumnModel
+    {
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [XmlIgnore]
+        public string Value { get; set; }
+
+        [XmlElement("value")]
+        public XmlCDataSection ValueCData
+        {
+            get => new XmlDocument().CreateCDataSection(Value);
+            set => Value = value.Value;
+        }
+
+    }
+
+    public enum HeaderColumnType
+    {
+        [XmlEnum("0")]
+        Type1 = 0,
+        [XmlEnum("Type2")]
+        Type2 = 1
+    }
+}
---
+++ DotNetStackLibraries/CSharp.XML.Serialize/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace CSharp.XML.Serialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FormModel form = null;
+            using (var fs = File.Open("Test.xml", FileMode.Open))
+            using (var sr = new StreamReader(fs, Encoding.UTF8))
+            {
+                var xmlSerializer = new XmlSerializer(typeof(FormModel));
+                form = (FormModel)xmlSerializer.Deserialize(sr);
+            }
+
+
+            form.Export.First().Body.Columns.First().Value = "将1111改为2222";
+
+            string xml = null;
+            var xmlWriterSettings = new XmlWriterSettings
+            {
+                Indent = true,
+                OmitXmlDeclaration = false,
+                Encoding = Encoding.UTF8
+            };
+            using (var ms = new MemoryStream())
+            {
+                using (var xmlWriter = XmlWriter.Create(ms, xmlWriterSettings))
+                {
+                    var xmlSerializer = new XmlSerializer(typeof(FormModel));
+                    // 同时去除form节点上的默认命名空间
+                    xmlSerializer.Serialize(xmlWriter, form, new XmlSerializerNamespaces(new[] { new XmlQualifiedName() }));
+                }
+
+                ms.Position = 0;
+                using (var sr = new StreamReader(ms))
+                {
+                    xml = sr.ReadToEnd();
+                }
+            }
+
+            Console.WriteLine(xml);
+            Console.ReadKey();
+
+        }
+    }
+}
